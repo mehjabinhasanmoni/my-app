0f5a4b4ef98377437309675712ef253bb43d6b4c
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2024-02-07 18:47:26 +0600

    one page created

---
+++ src/app/about-us/page.js
@@ -0,0 +1,14 @@
+"use client"
+import { useState } from "react";
+
+
+const page = () => {
+    const [counter,setCounter]=useState(0)
+    return (
+        <div style={{backgroundColor:"blue",height:"100vh"}}>
+            Hello about
+        </div>
+    );
+};
+
+export default page;
