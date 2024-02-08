ef85e6fc9fcfd427d4e9f408362196e911c67df2
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2024-02-08 23:37:57 +0600

    testing

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 const page = () => {
     const [counter,setCounter]=useState(0)
     return (
-        <div style={{backgroundColor:"blue",height:"100vh"}}>
+        <div style={{backgroundColor:"red",height:"100vh"}}>
             Hello about
             Testing
         </div>
